d1c87f529e353d04483c0e6d95671b627dd51d90
Ashithdeveloper <ashithashith593@gmail.com>
2025-06-07 22:43:44 +0530

    gemini-clone

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Main from './Component/Main/Main'
+import SideBar from './Component/SideBar/SideBar'
+
+function App() {
+ 
+  return (
+    <>
+     <SideBar/>
+     <Main/>
+    </>
+  )
+}
+
+export default App
